00e049035554b6e5bc2c3041a2aea7722f8c559e
CarolinaJavierEnriquez <carolinajavierenriquez@gmail.com>
2015-04-20 18:39:34 -0500

    creación de la sección citas y footer

--- jquery.js
+++ jquery.js
@@ -1,3 +1,15 @@
 $(".circulo-exterior").hover(function(){
 	$(this).toggleClass("text-circle-hover");
 })
+
+
+$(window).scroll(function(){
+	var prueba = $(this).scrollTop();
+	if (prueba > $(".menu").height()) {
+		$(".menu").addClass("menu-fixed");
+		
+	}
+		else if (prueba < 714) {
+		$(".menu").removeClass("menu-fixed");
+	}
+});
