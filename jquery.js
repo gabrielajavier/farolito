c50de731379a05328c3372d9889e84b32be243b3
CarolinaJavierEnriquez <carolinajavierenriquez@gmail.com>
2015-04-23 16:42:22 -0500

    solución del fixed en movil

--- jquery.js
+++ jquery.js
@@ -1,4 +1,23 @@
 
+/*MENU*/
+
+$(window).scroll(function(){
+	var prueba = $(this).scrollTop();
+	if (prueba > 714) {
+		$(".navbar").addClass("scroll");
+		$(".farolito-icon").addClass("scroll");
+		$(".icon-farolito-movil").addClass("scroll");
+	}
+		else if (prueba < 714) {
+		$(".navbar").removeClass("scroll");
+		$(".farolito-icon").removeClass("scroll");
+		$(".icon-farolito-movil").removeClass("scroll");
+	 }
+});
+
+
+
+/*CIRCULO METODOLOGIA*/
 
 $(document).ready(function() {
 	$(".circulo-exterior").hover(function(){
@@ -8,13 +27,3 @@ $(document).ready(function() {
 	});
 });
 
-
-$(window).scroll(function(){
-	var prueba = $(this).scrollTop();
-	if (prueba > 714) {
-		$(".navbar").addClass("scroll");
-	}
-		else if (prueba < 714) {
-		$(".navbar").removeClass("scroll");
-	 }
-});
